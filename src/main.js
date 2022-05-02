8b47b04fba4b16a5babd08029b65d8bfc68dee32
“Jane <ttran161@ucsc.edu>
2022-05-02 13:12:54 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 // HOW DANDY - Endless Runner
 // TEAM MEMBERS: Andrew Dresel-Kurtz, Ashley Perez, Jane Tran
-<<<<<<< HEAD
-// Date Complete:
-//
-// Creative: 
-//
-// Visual:
-=======
 // Date Complete: 5/2/2022
-// 
-// Technical Interesting: The randomization for the bird and power up object.  
+//
+// Technical Interesting: The randomization for the bird and power up object.
 // Visual: The theme of the game is really solid where the objective of the game is "true to life" and all the art follows a similar retro aesthetic.
->>>>>>> f1371611844a46d9723cf8c839d90c060a7b1815
+
 
 let config = {
     type: Phaser.AUTO,
